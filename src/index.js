8e882500618d426243ad1c04ec828c86e5499b09
Victor Pongnian <victor.pongnian@gmail.com>
2016-09-01 15:29:05 +0200

    refresh component + add eslint with airbnb guidelines

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDOM from 'react-dom'
+// Create a new component. this component should produce some HTML
 
-import App from './components/app';
-import reducers from './reducers';
+const App = () => {
+    return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this compnent's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
