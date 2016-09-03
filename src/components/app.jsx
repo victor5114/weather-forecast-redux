4f74f369ed2877d555e969e0ca404d94c45177d5
Victor Pongnian <victor.pongnian@gmail.com>
2016-09-03 23:09:57 +0200

    update init file + browser env

---
+++ src/components/app.jsx
@@ -0,0 +1,6 @@
+
+import React from 'react';
+
+const App = () => <div>React simple starter</div>;
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-// Create a new component. this component should produce some HTML
-const App = () => <div>Hi!</div>;
+import App from './components/app';
+import reducers from './reducers';
 
-// Take this compnent's generated HTML and put it on the page (in the DOM)
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-// /*eslint-env browser*/
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container')
+);
